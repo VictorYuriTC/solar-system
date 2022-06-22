27f679ffef94cba121631f1f1567bd617dafa032
Victor Yuri <victoryuritc@yahoo.com>
2022-06-22 11:18:46 -0300

    PlanetCard component added but not implemented to the App.

--- src/components/PlanetCard.js
+++ src/components/PlanetCard.js
@@ -4,14 +4,11 @@ import PropTypes from 'prop-types';
 class PlanetCard extends Component {
   render() {
     const it = this.props;
+    const alternativeText = `Planeta ${it.planetName}`;
     return (
       <div data-testid="planet-card">
-        <p data-testid="planet-name">
-          {it.planetName}
-        </p>
-        <div>
-          {it.planetImage}
-        </div>
+        <p>{it.planetName}</p>
+        <img src={ it.planetImage } alt={ alternativeText } />
       </div>
     );
   }
